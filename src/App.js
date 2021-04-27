1a09f3ce254d71573dfd64e95e1ebe1043e4b7d7
anderson <anderson697@hotmail.com>
2021-04-27 18:44:04 -0500

    update components menu-items withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepages/homePage.component';
+import { Route, Switch } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div>      
-      <HomePage />
+    <div>
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
