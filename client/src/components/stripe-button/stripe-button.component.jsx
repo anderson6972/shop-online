188b396eee7f5dac22d37f510061febf897e21a8
anderson <anderson697@hotmail.com>
2021-07-21 21:22:33 -0500

    client service included

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) =>{
+    const priceForStripe = price*100;
+    const publishableKey = 'pk_test_51IuqBJIiC0Mmk1gIBzcrwq9oljRZMmXFprZwiW3SaiNKYCrtv03P0QH8dvZfTCec2Mi2WtBTOdRKQAQSh77bT68x007Lgcv1O4';
+
+    const onToken= token =>{
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response =>{
+            alert('Payment successful')
+        }).catch(error =>{
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issues with your payment. Please sure you use the provided credit card');
+        })
+        // console.log(token);
+        // alert('Payment Succeful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clotinihg Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
